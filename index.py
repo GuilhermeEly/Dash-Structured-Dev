aaed8aa2c3a6cb32f9dfe9a89aaa47ace3ac6dec
Guilherme Ferreira Ely <gely@novusautomation.corp>
2020-07-22 09:23:05 -0300

    first

---
+++ index.py
@@ -0,0 +1,32 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from app import app
+from apps import app1, app2
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+index_page = html.Div([
+    dcc.Link('Go to Page 1', href='/apps/app1'),
+    html.Br(),
+    dcc.Link('Go to Page 2', href='/apps/app2'),
+])
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/apps/app1':
+        return app1.layout
+    elif pathname == '/apps/app2':
+        return app2.layout
+    else:
+        return index_page
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
