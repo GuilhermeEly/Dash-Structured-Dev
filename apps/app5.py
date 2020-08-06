14ca742da24fe42f0f5d63f30f49a9ab546fb950
Guilherme Ferreira Ely <gely@novusautomation.corp>
2020-08-06 15:12:45 -0300

    Filter that show true first passed approved products

--- apps/app5.py
+++ apps/app5.py
@@ -7,29 +7,25 @@ import pandas as pd
 dfo = pd.read_csv('apps\dataset\data.csv')
 dfo.columns =[column.replace(" ", "_") for column in dfo.columns]
 
-#dfoq = data.query('')
+##dfoq = data.query('')
 
 #Retorna todos equipamentos Aprovados em alguma das etapas.
 dfap = dfo.loc[(dfo['Status'] == "APROVADO")].drop_duplicates(subset = ["SERIAL_EQUIP"])
 
 #Retorna todas as reprovações únicas.
 dfrep = dfo.loc[(dfo['Status'] == "REPROVADO")].drop_duplicates(subset = ["SERIAL_EQUIP"])
+#Cria um novo dataframe com apenas a coluna SERIAL_EQUIP
+SNRep = dfrep.filter(['SERIAL_EQUIP'], axis=1)
+#Remove do dataframe de aprovados os produtos que aparecem na lista de reprovados, ficando apenas os que foram aprovados em todas as etapas
+#na primeira tentativa
+dfRlyApproved = dfap[~dfap.SERIAL_EQUIP.isin(SNRep.SERIAL_EQUIP)]
 
-
-dfmerge = pd.merge(dfap, dfrep, how='outer', indicator=True)
-print(dfmerge)
-print (dfmerge.loc[dfmerge._merge == 'left_only', ['SERIAL_EQUIP']])
-#Tentativa: Pegar o número de OP's únicos, verificar quais nomes de produtos únicos existem. Este número será considerado a 
+#Tentativa: Pegar o número de OP's únicas, verificar quais nomes de produtos únicos existem. Este número será considerado a 
 #quantidade de etapas de calibração existente. Então pegar o número de série e verificar se ele possui os "X" testes Aprovados
 
-dfremove = dfrep[['SERIAL_EQUIP']]
-print(dfremove)
 
-print(dfap.loc[~dfap.index.isin(dfap.merge(dfremove.assign(a='key'),how='left').dropna().index)])
-#Retorna um Numpy com a quantidade de equipamentos únicos reprovados por OP
-#print(df.drop_duplicates().ORDEM_PROD.value_counts())
 
-df = dfap
+df = dfRlyApproved
 
 layout = dash_table.DataTable(
     data=df.to_dict('records'),
