f5cef1d1ff7747892433a8aedf6dda2a4524df38
Guilherme Ferreira Ely <gely@novusautomation.corp>
2020-08-07 09:03:19 -0300

    Data treatment and formatting

--- apps/app5.py
+++ apps/app5.py
@@ -7,8 +7,11 @@ import pandas as pd
 dfo = pd.read_csv(r'apps\dataset\data.csv')
 dfo.columns =[column.replace(" ", "_") for column in dfo.columns]
 
-#Retorna todos equipamentos Aprovados em alguma das etapas.
-dfap = dfo.loc[(dfo['Status'] == "APROVADO")].drop_duplicates(subset = ["SERIAL_EQUIP"])
+#Transforma a coluna Data_Calibra em datetime
+dfo['Data_Calibra'] = pd.to_datetime(dfo['Data_Calibra'],format=r'%d/%m/%Y')
+
+#Transforma e formata a coluna Data_Calibra para uma STRING com o formato BR de data
+dfo['Data_Calibra'] = dfo['Data_Calibra'].dt.strftime(r'%d/%m/%Y')
 
 #Retorna todas as reprovações únicas.
 dfrep = dfo.loc[(dfo['Status'] == "REPROVADO")].drop_duplicates(subset = ["SERIAL_EQUIP"])
@@ -24,6 +27,8 @@ dfRlyApproved = dfo[~dfo.SERIAL_EQUIP.isin(SNRep.SERIAL_EQUIP)]
 SNRlyApproved = dfRlyApproved.filter(['SERIAL_EQUIP'], axis=1)
 dfRlyApproved_AllSteps = dfRlyApproved[dfRlyApproved.SERIAL_EQUIP.isin(SNRlyApproved.SERIAL_EQUIP)]
 
+#Retorna todos equipamentos Aprovados, apenas uma etapa por linha.
+dfap = dfRlyApproved_AllSteps.loc[(dfRlyApproved_AllSteps['Status'] == "APROVADO")].drop_duplicates(subset = ["SERIAL_EQUIP"])
 
 #Retorna todas as Etapas dos equipamentos reprovados, inclusive as aprovadas
 dfReproved_AllSteps = dfo[dfo.SERIAL_EQUIP.isin(SNRep.SERIAL_EQUIP)]
@@ -33,7 +38,7 @@ dfReproved_AllSteps = dfo[dfo.SERIAL_EQUIP.isin(SNRep.SERIAL_EQUIP)]
 #Tentativa: Pegar o número de OP's únicas, verificar quais nomes de produtos únicos existem. Este número será considerado a 
 #quantidade de etapas de calibração existente. Então pegar o número de série e verificar se ele possui os "X" testes Aprovados
 
-df = dfReproved_AllSteps
+df = dfap
 
 layout = dash_table.DataTable(
     data=df.to_dict('records'),
